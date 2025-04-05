ae8d062cd27d4bd1f35198a7831aa1cf0c3a60be
Hassanjavaid <hassanjavaid1214@gmail.com.com>
2025-04-05 11:41:14 +0500

    initial commit

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,15 +1,17 @@
 const mysql2 = require("mysql2");
-if (
-  (process.env.HOST === true,
-  process.env.USER === true,
-  process.env.PASSWORD === true,
-  process.env.DATABASE === true)
-) {
-  const db = mysql2.createConnection({
-    host: process.env.HOST || "",
-    user: process.env.USER || "",
-    password: process.env.PASSWORD || "",
-    database: process.env.DATABASE || "",
-  });
-  module.exports = db;
-}
+// if (
+//   (process.env.HOST === true,
+//   process.env.USER === true,
+//   process.env.PASSWORD === true,
+//   process.env.DATABASE === true)
+// ) {
+//   const db = mysql2.createConnection({
+//     host: process.env.HOST || "",
+//     user: process.env.USER || "",
+//     password: process.env.PASSWORD || "",
+//     database: process.env.DATABASE || "",
+//   });
+// }
+let db = true;
+
+module.exports = db;
